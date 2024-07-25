0fbf910597d6a85b112dbbaa3f6d80ab87d4359a
Deepak Yadav <deepak.yadav@snowflake.com>
2024-07-25 00:09:55 +0000

    few  more changes

--- 06_docker_jupyter_service.py
+++ 06_docker_jupyter_service.py
@@ -92,7 +92,7 @@ try:
 
     # USE ROLE CONTAINER_USER_ROLE;
     # CALL SYSTEM$REGISTRY_LIST_IMAGES('/CONTAINER_HOL_DB/PUBLIC/IMAGE_REPO');
-    images = root.databases["CONTAINER_HOL_DB"].schemas["PUBLIC"].image_repositories["IMAGE_REPO"].listImagesInRepository()
+    images = root.databases["CONTAINER_HOL_DB"].schemas["PUBLIC"].image_repositories["IMAGE_REPO"].list_images_in_repository()
     for image in images:
         print(image)
 
